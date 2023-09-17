7152486ef861879fdc6f39eb904f38e8615a1946
Francisco García <fco.garcia.delgado@gmail.com>
2023-09-17 22:33:50 +0200

    @EnableJpaRepositories

--- src/main/java/com/spring/peluqueria/SpringBootPeluqueriaApplication.java
+++ src/main/java/com/spring/peluqueria/SpringBootPeluqueriaApplication.java
@@ -3,10 +3,12 @@ package com.spring.peluqueria;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.spring.peluqueria.config.BezkoderProperties;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.spring.peluqueria.repository")
 @EnableConfigurationProperties(BezkoderProperties.class)
 public class SpringBootPeluqueriaApplication {
 
